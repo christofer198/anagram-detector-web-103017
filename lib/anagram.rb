ab1effd6e7e83c81d62675d2e679deef74aa81b6
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-05 22:05:46 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(phrase)
+    match_case = @word.downcase.split('').sort
+    binding.pry
+    #phrase_array = phrase.split
+  end
+
+end
+    
+  
